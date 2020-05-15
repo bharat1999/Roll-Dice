7b88ede137109a351144d673a8e763cb65b503a8
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2020-05-16 01:56:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var randomNumber1=Math.floor((Math.random()*6))+1;
+var randomNumber2=Math.floor((Math.random()*6))+1;
+document.querySelector("img.img1").setAttribute("src","images/dice"+ randomNumber1 + ".png");
+document.querySelector("img.img2").setAttribute("src","images/dice"+ randomNumber2 + ".png");
+if(randomNumber1>randomNumber2)
+    document.querySelector("h1").textContent="🚩Player 1 Wins!"
+else if(randomNumber2>randomNumber1)
+    document.querySelector("h1").textContent="Player 2 Wins!🚩"
+else
+    document.querySelector("h1").textContent="🚩Draw!🚩";    
+
+function reload()
+{
+    location.reload(true);
+}
