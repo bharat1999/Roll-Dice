927057d2441f66544e67dfd2d8ca63684448eca8
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2020-05-16 14:42:49 +0530

    Add files via upload
    added a roll button instead of refresh

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-var randomNumber1=Math.floor((Math.random()*6))+1;
-var randomNumber2=Math.floor((Math.random()*6))+1;
-document.querySelector("img.img1").setAttribute("src","images/dice"+ randomNumber1 + ".png");
-document.querySelector("img.img2").setAttribute("src","images/dice"+ randomNumber2 + ".png");
-if(randomNumber1>randomNumber2)
-    document.querySelector("h1").textContent="🚩Player 1 Wins!"
-else if(randomNumber2>randomNumber1)
-    document.querySelector("h1").textContent="Player 2 Wins!🚩"
-else
-    document.querySelector("h1").textContent="🚩Draw!🚩";    
-
-function reload()
+document.querySelector("button").onclick=roll;
+function roll()
 {
-    location.reload(true);
+    var randomNumber1=Math.floor((Math.random()*6))+1;
+    var randomNumber2=Math.floor((Math.random()*6))+1;
+    document.querySelector("img.img1").setAttribute("src","images/dice"+ randomNumber1 + ".png");
+    document.querySelector("img.img2").setAttribute("src","images/dice"+ randomNumber2 + ".png");
+    if(randomNumber1>randomNumber2)
+        document.querySelector("h1").textContent="🚩Player 1 Wins!"
+    else if(randomNumber2>randomNumber1)
+        document.querySelector("h1").textContent="Player 2 Wins!🚩"
+    else
+        document.querySelector("h1").textContent="🚩Draw!🚩";    
 }
